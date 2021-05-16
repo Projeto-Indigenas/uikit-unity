8e108c5d59c4e59bf6bb8e06deed975ecd2e4c27
Anderson Lucas C. Ramos <andersonlucasg3@hotmail.com>
2021-05-16 02:37:40 -0300

    Fixed dropdown event to action;

--- Assets/Runtime/Views/Components/DropdownView.cs
+++ Assets/Runtime/Views/Components/DropdownView.cs
@@ -7,7 +7,6 @@ using UnityEngine;
 
 namespace UIKit
 {
-
     [RequireComponent(typeof(TMP_Dropdown))]
     public class DropdownView : View
     {
@@ -15,7 +14,7 @@ namespace UIKit
         
         private TMP_Dropdown _dropdown = default;
 
-        [UsedImplicitly] public readonly DropdownEvent onValueChanged = new DropdownEvent(); 
+        [UsedImplicitly] public Action<DropdownOption> onValueChanged; 
 
         [UsedImplicitly]
         public void SetOptions(DropdownOption[] options) => _dropdown.options = options.Map(_mapper).AsList();
@@ -32,21 +31,6 @@ namespace UIKit
         private void ValueChangedAction(int index) => onValueChanged.Invoke((DropdownOption) _dropdown.options[index]);
     }
 
-    public class DropdownEvent
-    {
-        private readonly List<Action<DropdownOption>> _listeners = new List<Action<DropdownOption>>();
-
-        [UsedImplicitly] public void AddListener(Action<DropdownOption> action) => _listeners.Add(action);
-        [UsedImplicitly] public void RemoveListener(Action<DropdownOption> action) => _listeners.Remove(action);
-        [UsedImplicitly] public void RemoveAllListeners() => _listeners.Clear();
-
-        internal void Invoke(DropdownOption dropdownOption)
-        {
-            for (int index = 0; index < _listeners.Count; index++) 
-                _listeners[index].Invoke(dropdownOption);
-        }
-    }
-    
     public abstract class DropdownOption : TMP_Dropdown.OptionData
     {
         protected DropdownOption(string text = null, Sprite image = null) : base(text, image) { }
